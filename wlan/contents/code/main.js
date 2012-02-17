47e3627740f81caa8f44ab4644eda282be655f3c
Maik Beckmann <beckmann.maik@googlemail.com>
2012-02-17 23:15:53 +0100

    TextWlanMon from code subset of TextMon.

--- wlan/contents/code/main.js
+++ wlan/contents/code/main.js
@@ -2,7 +2,7 @@
 
 /** Javscript modules */
 if (!plasmoid.include("helpers.js")) {
-  throw("[CODE ERROR] couldn't load helpers.js module");
+  throw ("[CODE ERROR] couldn't load helpers.js module");
 }
 
 // Font properties
@@ -12,57 +12,70 @@ var styleSheet = { "font-family": "Liberation Mono",
                    "color": "white" };
 
 
-var sink = (function () {
-  var obj = {}
+/** wlan */
+var wlan = (function () {
+  var that = {};
 
-  obj.sourceUp = "network/interfaces/wlan0/transmitter/data";
-  obj.sourceDown = "network/interfaces/wlan0/receiver/data";
-  obj.cache = {
+  that.sourceDown = "network/interfaces/wlan0/receiver/data";
+  that.sourceUp = "network/interfaces/wlan0/transmitter/data";
+  that.sources = [that.sourceDown, that.sourceUp];
+  that.cache = {
     down: {value: "----", units: "KB/s"},
     up: {value: "----", units: "KB/s"}
   };
 
-  var layout = new LinearLayout(plasmoid);
   var label = new Label();
-  label.text =   obj.cache.down.value + obj.cache.up.units + " "
-               + obj.cache.up.value + obj.cache.down.units;
   label.styleSheet = helpers.styleSheetToString(styleSheet);
-  layout.addItem(label);
-  //
-  obj.layout = layout;
-  obj.label = label;
+  label.wordWrap = false;
+  label.text = "down:----KB/s up:----KB/s";
+  that.label = label;
 
-  obj.dataUpdated = function (name, data) {
-    if (!data["value"]) { return; }
+  that.dataUpdated = function (name, data) {
+    if (!data.value) { return; }
 
-    if (name == this.sourceDown) {
+    if (name === this.sourceDown) {
       this.cache.down = data;
-      this.cache.down["value"] = parseInt(data["value"], 10);
-    } else if (name == this.sourceUp) {
+      this.cache.down.value = parseInt(data.value, 10).toString();
+    } else if (name === this.sourceUp) {
       this.cache.up = data;
-      this.cache.up["value"] = parseInt(data["value"], 10);
+      this.cache.up.value = parseInt(data.value, 10).toString();
     }
 
-    var text = "up: " + this.cache.down["value"] + this.cache.down["units"];
+    var down = this.cache.down;
+    var paddedValue = helpers.padStrLeft(down.value, ' ', 4);
+    var text = "down:" + paddedValue + down.units;
+    //
     text += " ";
-    text += "down: " + this.cache.up["value"] + this.cache.up["units"];
+    //
+    var up = this.cache.up;
+    var paddedValue = helpers.padStrLeft(up.value, ' ', 4);
+    text += "up:" + paddedValue + up.units;
+
     this.label.text = text;
+  };
+
+  return that;
+}());
+
+
+/**
+  Connect sinks to their sources.
+*/
+
+/* Throws if connection fails */
+function checkedConnectSource(engine, source, sink) {
+  if (!engine.connectSource(source, sink, 1000)) {
+    throw ("connection attempt to '" + source + "' failed");
   }
-
-  return obj;
-})();
-
+}
 
 var engine = dataEngine("systemmonitor");
 
-if ( engine.connectSource(sink.sourceDown, sink, 1000) ) {
-  print("connection to '" +  sink.sourceDown + "' established");
-} else {
-  print("connection attempt to '" + sink.sourceDown + "' failed");
+for (var k in wlan.sources) {
+  checkedConnectSource(engine,  wlan.sources[k], wlan);
 }
 
-if ( engine.connectSource(sink.sourceUp, sink, 1000) ) {
-  print("connection to '" +  sink.sourceUp  + "' established");
-} else {
-  print("connection attempt to '" + sink.sourceUp + "' failed");
-}
+
+/** layout */
+var layout = new LinearLayout(plasmoid);
+layout.addItem(wlan.label);
