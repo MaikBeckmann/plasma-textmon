a5e094f888853bef71c52e13a0e2770332c04290
Maik Beckmann <beckmann.maik@googlemail.com>
2012-02-17 23:32:01 +0100

    TextCpuMon from code subset of TextMon.

--- cpu/contents/code/main.js
+++ cpu/contents/code/main.js
@@ -5,48 +5,49 @@ if (!plasmoid.include("helpers.js")) {
   throw("[CODE ERROR] couldn't load helpers.js module");
 }
 
-// Font properties
+
+/** Font properties */
 var styleSheet = { "font-family": "Liberation Mono",
                    "font-style": "normal",
                    "font-size": "10px",
                    "color": "white" };
 
 
-var observer = (function () {
-  var obj = {};
+/** cpu */
+var cpu = (function () {
+  var that = {};
 
-  obj.source = "cpu/system/TotalLoad";
-  obj.interval = 1000;;
-  obj.observers = {};
-  obj.data = {value: "---", units: "%"};
+  that.source = "cpu/system/TotalLoad";
+  that.sources = [that.source];
 
-
-  var layout = new LinearLayout(plasmoid);
   var label = new Label();
-  label.text = "---%";
   label.styleSheet = helpers.styleSheetToString(styleSheet);
-  layout.addItem(label);
-  //
-  obj.layout = layout;
-  obj.label = label;
+  label.wordWrap = false;
+  label.text = "---%";
+  that.label = label;
 
-
-  obj.dataUpdated = function (name, data) {
+  that.dataUpdated = function (name, data) {
     if (!data.value) { return; }
     if (name !== this.source) {
-      throw("[CODE ERROR] this sink is meant to handle '" + this.source +
-	    "', not '" + name + "'");
+      throw ("[CODE ERROR] this routine is meant to handle '"
+	     + this.source + "', not '" + name + "'");
     }
 
-    this.label.text = parseInt(data.value, 10) + data.units;
+    var paddedValue = helpers.padStrLeft(parseInt(data.value, 10).toString(), ' ', 3);
+    this.label.text = paddedValue + data.units;
   };
 
-  return obj;
-})();
+  return that;
+}());
 
 
 var engine = dataEngine("systemmonitor");
 
-if (!engine.connectSource(observer.source, observer, observer.interval)) {
-  throw("connection attempt to '" + observer.source +"' failed");
+if (!engine.connectSource(cpu.source, cpu, 1000)) {
+  throw("connection attempt to '" + cpu.source +"' failed");
 }
+
+
+/** layout */
+var layout = new LinearLayout(plasmoid);
+layout.addItem(cpu.label);
