d06b2c6a2742ba14ccf25c603519a8caefb169fc
Maik Beckmann <beckmann.maik@googlemail.com>
2012-02-17 09:41:17 +0100

    toying with observer pattern.

--- cpu/contents/code/main.js
+++ cpu/contents/code/main.js
@@ -1,10 +1,9 @@
 // -*- coding: utf-8 -*-
 
-var layout = new LinearLayout(plasmoid);
-var label = new Label();
-label.text = "---%";
-layout.addItem(label);
-
+/** Javscript modules */
+if (! plasmoid.include("helpers.js")) {
+  throw("[CODE ERROR] couldn't load helpers.js module");
+}
 
 // Font properties
 var styleSheet = { "font-family": "Liberation Mono",
@@ -12,11 +11,41 @@ var styleSheet = { "font-family": "Liberation Mono",
                    "font-size": "10px",
                    "color": "white" };
 
+
+var view = (function () {
+  var obj = {};
+
+  var layout = new LinearLayout(plasmoid);
+  var label = new Label();
+  label.text = "---%";
+  label.styleSheet = helpers.styleSheetToString(styleSheet);
+
+  layout.addItem(label);
+
+  obj.layout = layout;
+  obj.label = label;
+  obj.model = null;
+
+  obj.dataUpdated = function () {
+    if (!this.model) {
+      throw("[CODE ERROR] Model isn't set.");
+    }
+    var data = this.model.data;
+    this.label.text = data.value + data.units;
+  }
+
+  return obj;
+
+})();  
+
+
 var sink = (function () {
-  var obj = {}
+  var obj = {};
 
   obj.source = "cpu/system/TotalLoad";
-  obj.interval = 1000;
+  obj.interval = 1000;;
+  obj.observers = {};
+  obj.data = {value: "---", units: "%"};
 
   obj.dataUpdated = function (name, data) {
     if (!data["value"]) { return; }
@@ -25,9 +54,15 @@ var sink = (function () {
 	    "', not '" + name + "'");
     }
     
-    var msg = parseInt(data["value"], 10) + data["units"];
-    print(msg);
-  }
+    for(var k in this.observers) {
+      var observer = this.observers[k];
+      if (typeof observer.dataUpdated !== 'function') {
+	throw("[CODE ERROR] observer hasn't got the dataUpdated slot");
+      }
+      this.data.value = parseInt(data.value, 10);
+      observer.dataUpdated();
+    }    
+  };
 
   return obj;
 })();
@@ -36,4 +71,8 @@ var sink = (function () {
 var engine = dataEngine("systemmonitor");
 
 if (! engine.connectSource(sink.source, sink, sink.interval) ) {
-  throw("connection attempt to '" + sink.source +"' failed"); }
+  throw("connection attempt to '" + sink.source +"' failed"); 
+}
+
+sink.observers["label"] = view;
+view.model = sink;
